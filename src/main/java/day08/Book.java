dabdcc6abbfb57c4cc702b89db4550d745c51bb1
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-27 15:06:15 +0800

    Add files via upload

---
+++ src/main/java/day08/Book.java
@@ -0,0 +1,33 @@
+package day08;
+
+/*
+書 Book
+---------------------------------
+書名: name
+價格: price
+---------------------------------
+取得書名的方法: getName()
+設定書名的方法: setName(String name)
+取得價格的方法: getPrice()
+設定價格的方法: setPrice(int price)
+*/
+public class Book {
+    private String name;
+    private int price;
+    
+    // 方法封裝
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(int price) {
+        if(price >= 0) {
+            this.price = price;
+        } 
+    }
+}
