6ef0f90ccbc9230d09cde6579780958768ad7b3e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-27 15:20:28 +0800

    Add files via upload

---
+++ src/main/java/day08/BookTest.java
@@ -0,0 +1,14 @@
+package day08;
+
+public class BookTest {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        b1.setName("Java");
+        b1.setPrice(200);
+        
+        Book b2 = new Book("Python", 150);
+        
+        System.out.println(b1.getName() + " $" + b1.getPrice());
+        System.out.println(b2.getName() + " $" + b2.getPrice());
+    }
+}
