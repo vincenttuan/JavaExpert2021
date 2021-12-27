123bc9dda4195d69774f9a6ff0be0e6b7637de05
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-27 16:23:06 +0800

    Add files via upload

---
+++ src/main/java/day08/BookStoreTest.java
@@ -0,0 +1,28 @@
+package day08;
+
+import java.util.List;
+
+public class BookStoreTest {
+    public static void main(String[] args) {
+        // 書店
+        BookStore bookStore = new BookStore();
+        
+        // 書籍新增(上架)
+        bookStore.addBook("Java", 200);
+        bookStore.addBook("Python", 150);
+        bookStore.addBook("C++", 100);
+        
+        // 查詢所有書籍
+        List<Book> books = bookStore.findAllBooks();
+        // 印出所有書籍
+        System.out.println(books);
+        // 該書店的書籍共有幾本
+        System.out.println(books.size());
+        
+        // 修改 Java 的價格為 500 元
+        bookStore.updateBookPriceByName("Java", 500);
+        // 重新找到並印出目前所有書籍
+        System.out.println(bookStore.findAllBooks());
+        
+    }
+}
