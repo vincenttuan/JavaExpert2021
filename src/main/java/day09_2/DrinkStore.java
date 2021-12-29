008d1d63eaeabb4239afc63f3e8b5ec6a408f9d9
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-29 14:21:07 +0800

    Add files via upload

--- src/main/java/day09_2/DrinkStore.java
+++ src/main/java/day09_2/DrinkStore.java
@@ -11,8 +11,8 @@ public class DrinkStore {
         Drink tea      = new Drink(250, "Tea", 20);
         Drink milk     = new Drink(500, "Milk", 50);
         Drink coffee   = new Drink(400, "Coffee", 70);
-        Dessert cake   = new Dessert("小", "Cake", 35);
-        Dessert cookie = new Dessert("大", "Cookie", 15);
+        Dessert cake   = new Dessert("S", "Cake", 35);
+        Dessert cookie = new Dessert("L", "Cookie", 15);
         // 加入到商品集合中
         products.add(tea);
         products.add(milk);
@@ -29,11 +29,22 @@ public class DrinkStore {
         System.out.println("+---------+------+----------+---------+-------+");
         System.out.println("|  Type   | Size | Capacity |   Name  | Price |");
         System.out.println("+---------+------+----------+---------+-------+");
+        
         for (Product product : products) {
+            String size = "";
+            String capacity = "";
+            switch(product.getClass().getSimpleName()) {
+                case "Drink":
+                    capacity = ((Drink)product).getCapacity() + "";
+                    break;
+                case "Dessert":
+                    size = ((Dessert)product).getSize();
+                    break;
+            }
             System.out.printf("| %-7s | %4s | %-8s | %-7s | %5s |\n", 
                               product.getClass().getSimpleName(),
-                              "",
-                              "",
+                              size,
+                              capacity,
                               product.getName(),
                               product.getPrice());
             System.out.println("+---------+------+----------+---------+-------+");
