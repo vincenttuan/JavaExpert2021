24badb8722b73ab060976fd331ef88e6f8ee89ab
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-29 14:03:25 +0800

    Add files via upload

---
+++ src/main/java/day09_2/DrinkStore.java
@@ -0,0 +1,51 @@
+package day09_2;
+
+import java.util.ArrayList;
+
+public class DrinkStore {
+    // 商品集合
+    private ArrayList<Product> products = new ArrayList<>();
+    
+    public DrinkStore() {
+        // 預設商品
+        Drink tea      = new Drink(250, "Tea", 20);
+        Drink milk     = new Drink(500, "Milk", 50);
+        Drink coffee   = new Drink(400, "Coffee", 70);
+        Dessert cake   = new Dessert("小", "Cake", 35);
+        Dessert cookie = new Dessert("大", "Cookie", 15);
+        // 加入到商品集合中
+        products.add(tea);
+        products.add(milk);
+        products.add(coffee);
+        products.add(cake);
+        products.add(cookie);
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+    
+    public void print() {
+        // Drink{capacity=250, name=Tea, price=20}
+        // Dessert{size=大, name=Cookie, price=15
+        System.out.println("+---------+------+----------+---------+-------+");
+        System.out.println("|  Type   | Size | Capacity |   Name  | Price |");
+        System.out.println("+---------+------+----------+---------+-------+");
+        for (Product product : products) {
+            System.out.printf("| %7s | %4s | %8s | %7s | %5s |\n", 
+                              product.getClass().getSimpleName(),
+                              "",
+                              "",
+                              product.getName(),
+                              product.getPrice());
+            System.out.println("+---------+------+----------+---------+-------+");
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return "DrinkStore{" + "products=" + products + '}';
+    }
+    
+    
+}
---
+++ src/main/java/day09_2/Test.java
@@ -0,0 +1,8 @@
+package day09_2;
+
+public class Test {
+    public static void main(String[] args) {
+        DrinkStore ds = new DrinkStore();
+        ds.print();
+    }
+}
