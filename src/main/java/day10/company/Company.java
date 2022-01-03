244ea428e97750b4f303232baa3be48f665b31c9
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-03 11:39:00 +0800

    Add files via upload

---
+++ src/main/java/day10/company/Company.java
@@ -0,0 +1,20 @@
+package day10.company;
+
+import java.util.ArrayList;
+
+public class Company {
+    private ArrayList<Employee> employees = new ArrayList<>();
+    
+    // 新增員工
+    public void add(Employee employee) {
+        employees.add(employee);
+    }
+    
+    // 印出每位員工的薪資與工作內容
+    public void print() {
+        for(Employee employee : employees) {
+            System.out.printf("薪資：%d 工作內容：", employee.getSalary());
+            employee.job();
+        }
+    }
+}
