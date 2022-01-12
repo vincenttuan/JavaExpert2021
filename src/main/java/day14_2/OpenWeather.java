a6f1d2fa8d978037d8c9b60e340b5ecf618dc1e7
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-12 15:56:24 +0800

    Add files via upload

--- src/main/java/day14_2/OpenWeather.java
+++ src/main/java/day14_2/OpenWeather.java
@@ -1,17 +1,46 @@
 package com.study.day14_2;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.TimeZone;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class OpenWeather {
-	public static void main(String[] args) throws Exception {
+	public static String getJsonString(String city) throws Exception {
 		String code = "8d1e6f10632315abbb1c2a2d796b7f0c";
-		String city = "taoyuan";
 		String urlstr = "https://api.openweathermap.org/data/2.5/weather?q=%s,tw&appid=%s";
 		urlstr = String.format(urlstr, city, code);
 		System.out.println(urlstr);
 		URL url = new URL(urlstr);
 		String jsonstr = new Scanner(url.openStream()).useDelimiter("\\A").next();
-		System.out.println(jsonstr);
+		//System.out.println(jsonstr);
+		return jsonstr;
 	}
+	
+	public static void main(String[] args) throws Exception {
+		String jsonstr = getJsonString("taoyuan");
+		System.out.println(jsonstr);
+		JsonElement je = JsonParser.parseString(jsonstr);
+		JsonObject root = je.getAsJsonObject();
+		Integer dt = root.get("dt").getAsInt();
+		String name = root.get("name").getAsString();
+		String main = root.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+		String description = root.getAsJsonArray("weather").get(0).getAsJsonObject().get("description").getAsString();
+		Double temp = root.getAsJsonObject("main").get("temp").getAsDouble();
+		Double feels_like = root.getAsJsonObject("main").get("feels_like").getAsDouble();
+		Integer humidity = root.getAsJsonObject("main").get("humidity").getAsInt();
+		
+		// 時間轉換
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String dt_str = sdf.format(new Date((long)dt*1000));
+		System.out.printf("時間：%d(%s)\n地區：%s\n天氣：%s\n天氣描述：%s\n氣溫：%.2f°C\n體感：%.2f°C\n濕度：%d%%\n",
+						  dt, dt_str, name, main, description, temp, feels_like, humidity);
+	}
+	
 }
